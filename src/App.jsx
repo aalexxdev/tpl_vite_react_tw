5d4395e523d51ff3d7d789d387ac56071cb1e1a6
L Ξ X <5699331+aalexxdev@users.noreply.github.com>
2025-09-15 19:56:38 +0200

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import { LoginForm } from "@/components/login-form";
 import { ModeToggle } from "@/components/mode-toggle";
 import "./App.css";
 import LanguageSwitcher from "@/components/language-switcher";
+import LetterGlitch from '@/components/LetterGlitch';
+
 function App() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <>
+    <div className="z-10 bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <ModeToggle /><LanguageSwitcher />
-      <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="w-full max-w-sm md:max-w-3xl z-10">
         <LoginForm />
       </div>
-    </div>
+    </div><LetterGlitch
+  glitchSpeed={100}
+  centerVignette={true}
+  outerVignette={true}
+  smooth={true}
+/></>
   );
 }
 
--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -10,7 +10,7 @@ export function LoginForm({ className, ...props }) {
   const { t } = useTranslation();
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn("flex flex-col gap-6 z-1", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <form className="p-6 md:p-8">

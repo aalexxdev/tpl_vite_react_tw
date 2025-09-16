dc1362441da7907d1443ca9e80638d56917bfdba
L Ξ X <5699331+aalexxdev@users.noreply.github.com>
2025-09-16 15:55:32 +0200

    updt icons

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -126,7 +126,7 @@ export function LoginForm({ className, ...props }) {
 
           <div className="relative hidden bg-muted md:block">
             <img
-              src="/placeholder.svg"
+              src="/images/placeholder.svg"
               alt={t("image.alt", "Image")}
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
